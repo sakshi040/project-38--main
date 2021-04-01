7a6c5e6a65ccf8fd75c25c6e6234125bd7e51a3d
sakshi040 <73293651+sakshi040@users.noreply.github.com>
2021-04-01 14:56:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var canvas;
+var gameState=0;
+var playerCount;
+var database;
+var form;
+var game;
+var player;
+var allPlayers;
+var car1;
+var car2;
+var car3;
+var car4;
+var cars;
+var blackImage;
+var blueImage;
+var pinkImage;
+var redImage;
+
+function preload(){
+blackImage=loadImage("black.png")
+blueImage=loadImage("blue.png")
+pinkImage=loadImage("pink.png")
+redImage=loadImage("red.png")
+}
+
+function setup(){
+    canvas=createCanvas(displayWidth-20,displayHeight-30)
+    database=firebase.database()
+    game=new Game()
+    game.getState();
+    game.start()
+}
+
+function draw(){
+    if(playerCount===4){
+game.update(1)
+    }
+    if(gameState===1){
+clear();
+game.play()
+    }
+    
+    
+}
